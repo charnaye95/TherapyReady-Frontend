b6f2e59f7efb575e4decc8651b8717dc51da1656
Charnaye Grier <charnayegrier@gmail.com>
2022-07-08 23:53:24 -0400

    working to get data to render from backend, trying axios next

--- therapy-ready/src/App.js
+++ therapy-ready/src/App.js
@@ -1,10 +1,31 @@
 // import logo from './logo.svg';
 import './App.css';
+import { Navigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
+
+  const [clinics, setClinics] = useState(null)
+  const [therapists, setTherapists] = useState(null)
+  const clinicurl = "https://therapyready-backend.herokuapp.com/clinics/"
+  const therapisturl = "https://therapyready-backend.herokuapp.com/therapists/"
+
+  function getClinics() {
+    fetch(clinicurl)
+      .then((res) => res.json())
+      .then((res) => setClinics(res))
+      .catch(console.error);
+  }
+
+  useEffect(() => {
+    getClinics()
+  }, [])
+
   return (
     <div className="App">
        Hello World
+      {clinics}
     </div>
   );
 }
