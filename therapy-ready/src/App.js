07e5de0119547796ed4c19d4b3ecd3bfa3bae64b
Charnaye Grier <charnayegrier@gmail.com>
2022-07-12 17:14:29 -0400

    tested out tailwindcss and had it render, but trying to create my own navbar now also setting up structure of app with components and pages

--- therapy-ready/src/App.js
+++ therapy-ready/src/App.js
@@ -1,8 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
-import { Navigate } from "react-router-dom";
+import './index.css';
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
+import Clinics from './Pages/Clinics';
+import Therapists from './Pages/Therapists';
+// import { Navigate } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
+import axios from 'axios';
 
 function App() {
 
@@ -12,20 +18,46 @@ function App() {
   const therapisturl = "https://therapyready-backend.herokuapp.com/therapists/"
 
   function getClinics() {
-    fetch(clinicurl)
-      .then((res) => res.json())
-      .then((res) => setClinics(res))
+    axios.get(clinicurl)
+      .then((res) => setClinics(res.data))
       .catch(console.error);
+  }
+
+  function getTherapists() {
+    axios.get(therapisturl)
+      .then((res) => setTherapists(res.data))
+      .catch(console.error);
+
   }
 
   useEffect(() => {
     getClinics()
+    getTherapists()
   }, [])
 
+  if (!clinics && !therapists) return <h1>"No data rendered"</h1>
+
+  
   return (
     <div className="App">
-       Hello World
-      {clinics}
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/clinics" element={<Clinics />} />
+        <Route path="/therapists" element={<Therapists />} />
+      </Routes>
+      <p>{clinics.map(clinic => 
+      <div>
+        {clinic.name} 
+        
+      </div>)}
+      </p>
+      <br></br>
+      <p>{therapists.map(therapist =>
+      <div>
+         {therapist.name} 
+         </div>
+         )}</p>
     </div>
   );
 }
