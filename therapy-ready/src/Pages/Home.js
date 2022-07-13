9835b397cb1d96b15735c42bda4d443421584921
Charnaye Grier <charnayegrier@gmail.com>
2022-07-12 20:17:31 -0400

    completed mockup of homepage

--- therapy-ready/src/Pages/Home.js
+++ therapy-ready/src/Pages/Home.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div className='searchbar'> ____________________ Search</div>
-  )
+    <div>
+      <div className="searchbar">
+        <h1>
+          ____________________<button className="search-button">Search</button>
+        </h1>
+      </div>
+
+      <div>
+        <br></br>
+        <h2 className="featured">Featured Clinics</h2>
+        <br></br>
+
+        <h2 className="featured">Featured Therapists</h2>
+      </div>
+      <br></br>
+      <Link to="/assessments">
+        <div className="assessment-button">
+          <button Navigate>
+            <p>Take one of our many assessments!</p>
+          </button>
+        </div>
+      </Link>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
